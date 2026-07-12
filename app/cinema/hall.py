e6bd123bf79863595782a71c4939af7f1d3114b4
milokc <alanmilok@gmail.com>
2026-07-12 20:23:04 +0300

    fixed outer quotes to avoid escaping inner quotes

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -9,10 +9,10 @@ class CinemaHall:
                       movie_name: str,
                       customers: list,
                       cleaning_staff: Cleaner) -> None:
-        print(f"\"{movie_name}\" started in hall number {self.number}.")
+        print(f'"{movie_name}" started in hall number {self.number}.')
 
         for customer in customers:
             customer.watch_movie(movie_name)
 
-        print(f"\"{movie_name}\" ended.")
+        print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.number)
